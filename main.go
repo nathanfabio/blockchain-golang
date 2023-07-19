85b684370201be381ad1f740b4a6c339bfbaeb90
Nathan Fabio <nathan_3_m@hotmail.com>
2023-07-18 21:10:33 -0300

    some changes

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"crypto/sha256"
 )
 
+type BlockChain struct {
+	blocks []*Block
+}
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -15,4 +18,20 @@ func (b *Block) DeriveHash() {
 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func main() {
+
 }
